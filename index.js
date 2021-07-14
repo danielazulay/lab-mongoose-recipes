c0d803b278880bee2d3c6ae0d91204359c3c65cc
Daniel Azulay <you@example.comdanielazulay20@gmail.com>
2021-07-14 20:19:38 -0300

    done

--- index.js
+++ index.js
@@ -19,8 +19,33 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
+  .then(async(db) => {
     // Run your code here, after you have insured that the connection was made
+const recipe = await Recipe.create({
+
+  title:'tomator soup',
+  level : 'Easy Peasy',
+  ingredients :["water","salt", "tomator"],
+  cuisine: "American",
+  dishType :"soup",
+  image :"https://www.inspiredtaste.net/wp-content/uploads/2018/10/Homemade-Vegetable-Soup-Recipe-2-1200.jpg",
+  duration :30,
+  creator :"daniel",
+
+})
+
+
+const response= await Recipe.insertMany(data)
+
+const restore= await Recipe.findOneAndUpdate({title:"Rigatoni alla Genovese"},
+  { $set: { duration: 100 } }, { new: true }  )
+
+  const del= await Recipe.deleteOne({title:"Carrot Cake"} )
+  console.log("create many",response);  
+  console.log("update one",restore);  
+console.log("del====>",del);  
+
+conexao.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -3,6 +3,16 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
   // TODO: write the schema
+
+  title:{type: String},
+level : {type: String ,enum:["Easy Peasy","Amateur Chef","UltraPro Chef",]},
+ingredients :[String],
+cuisine : [String],
+dishType :{type: String ,enum:["breakfast","main_course","soup","snack","drink","dessert","other"]},
+image :{type :String, Default: 'https://images.media-allrecipes.com/images/75131.jpg'},
+duration :{type: Number , min:0},
+creator :{type: String},
+created :{type :String, Default:"today"},
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
